022e9732198c467588e425debdb0e85a3da3de62
YuseiYokoyama <yusei_yokoyama@pkshatech.com>
2022-06-18 11:36:30 +0900

    rename

--- lcm/structure.py
+++ lcm/structure.py
@@ -7,17 +7,17 @@ class Itemset(set): # item id starts from 0
 
 class ItemsetPattern:
 
-    def __init__(self, freqency, items, hit):
-        self.freqency = int(freqency)
+    def __init__(self, freq, items, hit):
+        self.freq = int(freq)
         self.items = items
         self.hit = hit # contains itemset ids which have this pattern # itemset id starts from 0
 
     def __repr__(self):
-        return f"(freq:{self.freqency}, items:{self.items}, hit:{self.hit})"
+        return f"(freq:{self.freq}, items:{self.items}, hit:{self.hit})"
 
     def __eq__(self, other):
         conditions = (
-                self.freqency == other.freqency,
+                self.freq == other.freq,
                 self.items == other.items,
                 self.hit == other.hit,
                 )
