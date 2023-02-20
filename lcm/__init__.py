ff4db8165d8d06dd5e0ebdab9f23e574353f4d7a
YuseiYokoyama <yusei_yokoyama@pkshatech.com>
2023-02-20 14:11:38 +0900

    check lcm installed

--- lcm/__init__.py
+++ lcm/__init__.py
@@ -1,3 +1,9 @@
 from .main import run, run_auto, FailedAutoMinsupError
 from .adapter import NoFrequentItemError
 from .structure import Itemset, ItemsetPattern
+
+import shutil
+
+if shutil.which("lcm") is None:
+    raise FileNotFoundError("lcm command is not found. Please install lcm according to README.")
+
